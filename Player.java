3d4d4ee7b4d29fde211e6ac461e8a8c9abc590c7
Sonu64 <52707495+Sonu64@users.noreply.github.com>
2019-12-21 23:23:53 +0530

    modify move() to start on '1'.

--- Player.java
+++ Player.java
@@ -13,7 +13,7 @@ public class Player{
             this.name = name;
         else
             this.name = "Guest";
-        if(currentPosition==0.0 && currentPosition<=100.0)
+        if(currentPosition==1.0 && currentPosition<=100.0)
             this.currentPosition = currentPosition;
         else
             this.currentPosition = 0;
@@ -32,23 +32,40 @@ public class Player{
         int result = (int)(Math.random()*6) + 1;
         return result;
     }
-    public void move(int currentPosition)
+    public void move(int currentPosition, String currentSituation)
     {
         double diceResult = getDiceResult();
         System.out.println("Dice Result for " + name + " = " + diceResult);
-        currentPosition = (int)(currentPosition + diceResult);
-        this.currentPosition = currentPosition;
+        
+        if(diceResult==1 && currentSituation.equals("stillInHome"))
+        {
+            currentPosition = (int)(currentPosition + 1.0);
+            this.currentPosition = currentPosition;
+        }
+        else if(diceResult>=1 && currentSituation.equals("outFromHome"))
+        {
+            currentPosition = (int)(currentPosition + diceResult);
+            this.currentPosition = currentPosition;           
+        }
+        else if(diceResult>1 && currentSituation.equals("stillInHome"))
+        {
+            currentPosition = (int)(currentPosition + 0.0);
+            this.currentPosition = currentPosition;            
+        }
+        else //emergency condition, this condition is never true generally
+        {
+            currentPosition = 0;
+            this.currentPosition = currentPosition;
+        }
+        
     }
     public void climbLadder(int currentPosition, int increaseAmount)
-    {
-        
-        
+    {        
         currentPosition = currentPosition + increaseAmount;
         this.currentPosition = currentPosition;
     }
     public void downViaSnake(int currentPosition, int decreaseAmount)
-    {
-        
+    {       
         currentPosition = currentPosition - decreaseAmount;
         this.currentPosition = currentPosition;       
     }
